876b4e7cf0be5c204ba880916263c5069d6083da
Sonu Bhatt <itssonubhatt@gmail.com>
2023-07-17 00:50:01 +0530

    realtor and listing added in admin panel

--- realtors/admin.py
+++ realtors/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Realtor
+
+class RealtorAdmin(admin.ModelAdmin):
+  list_display = ('id', 'name', 'phone', 'email', 'is_mvp', 'hire_date')
+  list_display_links = ('id', 'name')
+  list_editable = ('is_mvp', )
+  search_fields = ('name', 'phone', 'email')
+  list_per_page = 20
 
 # Register your models here.
+admin.site.register(Realtor, RealtorAdmin)
